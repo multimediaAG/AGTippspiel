9253f2a36455877b8f14bd001f4edc31cf89fa78
Hannes Rüger <hannesrueger@gmx.de>
2021-05-31 18:32:46 +0200

    (api) fixed linting errors

--- api/src/controllers/MatchController.ts
+++ api/src/controllers/MatchController.ts
@@ -103,65 +103,59 @@ export class MatchController {
     }
 
     public static getMatches = async (req: Request, res: Response) => {
-        try {
-
-            const user = await getRepository(User).findOne(res.locals.jwtPayload.userId);
-            const tipRepository = getRepository(Tip);
-            const myTips = await tipRepository.find({where: { user }});
-            const allExpertTips = await tipRepository.createQueryBuilder('tip')
-            .innerJoinAndSelect('tip.user', 'user')
-            .where('user.isExpert = true').getMany();
-            console.log(allExpertTips)
-            res.send(MatchController.matches.map((m) => {
-                const match = copy(m) as Match;
-                const expertTipps = allExpertTips.filter((t) => t.matchId == match.id);
-                const tip = myTips.find((t) => t.matchId == match.id);
-                if (tip) {
-                    match.myTip.homeTeam = tip.scoreHomeTeam;
-                    match.myTip.awayTeam = tip.scoreAwayTeam;
+        const user = await getRepository(User).findOne(res.locals.jwtPayload.userId);
+        const tipRepository = getRepository(Tip);
+        const myTips = await tipRepository.find({where: { user }});
+        const allExpertTips = await tipRepository.createQueryBuilder('tip')
+        .innerJoinAndSelect('tip.user', 'user')
+        .where('user.isExpert = true').getMany();
+        res.send(MatchController.matches.map((m) => {
+            const match = copy(m) as Match;
+            const expertTipps = allExpertTips.filter((t) => t.matchId == match.id);
+            const tip = myTips.find((t) => t.matchId == match.id);
+            if (tip) {
+                match.myTip.homeTeam = tip.scoreHomeTeam;
+                match.myTip.awayTeam = tip.scoreAwayTeam;
+            }
+            
+            const expertCountHomeTeam = expertTipps.filter((t) => t.scoreAwayTeam < t.scoreHomeTeam).length;
+            const expertCountAwayTeam = expertTipps.filter((t) => t.scoreAwayTeam > t.scoreHomeTeam).length;
+            const expertCountDraw = expertTipps.filter((t) => t.scoreAwayTeam == t.scoreHomeTeam).length;
+            const expertCountTotal = expertCountHomeTeam + expertCountDraw + expertCountAwayTeam;
+            
+            match.expertOdds = {
+                points: {
+                    homeTeam: 10 * (expertCountDraw + expertCountAwayTeam) / expertCountTotal,
+                    draw: 10 * (expertCountHomeTeam + expertCountAwayTeam) / expertCountTotal,
+                    awayTeam: 10 * (expertCountHomeTeam + expertCountDraw) / expertCountTotal,
+                },
+                count: {
+                    homeTeam: expertCountHomeTeam,
+                    draw: expertCountDraw,
+                    awayTeam: expertCountAwayTeam,
                 }
-                
-                const expertCountHomeTeam = expertTipps.filter((t) => t.scoreAwayTeam < t.scoreHomeTeam).length;
-                const expertCountAwayTeam = expertTipps.filter((t) => t.scoreAwayTeam > t.scoreHomeTeam).length;
-                const expertCountDraw = expertTipps.filter((t) => t.scoreAwayTeam == t.scoreHomeTeam).length;
-                const expertCountTotal = expertCountHomeTeam + expertCountDraw + expertCountAwayTeam;
-                
-                match.expertOdds = {
-                    points: {
-                        homeTeam: 10 * (expertCountDraw + expertCountAwayTeam) / expertCountTotal,
-                        draw: 10 * (expertCountHomeTeam + expertCountAwayTeam) / expertCountTotal,
-                        awayTeam: 10 * (expertCountHomeTeam + expertCountDraw) / expertCountTotal,
+            };
+            match.expertTips = Object.fromEntries(expertTipps.map((expertTip) => {
+                return [
+                    expertTip.user.id,
+                    {
+                        homeTeam: expertTip.scoreHomeTeam,
+                        awayTeam: expertTip.scoreAwayTeam
                     },
-                    count: {
-                        homeTeam: expertCountHomeTeam,
-                        draw: expertCountDraw,
-                        awayTeam: expertCountAwayTeam,
-                    }
-                };
-                match.expertTips = Object.fromEntries(expertTipps.map((expertTip) => {
-                    return [
-                        expertTip.user.id,
-                        {
-                            homeTeam: expertTip.scoreHomeTeam,
-                            awayTeam: expertTip.scoreAwayTeam
-                        },
-                    ];
-                }));
-                return match;
+                ];
             }));
-        } catch (e) {
-            console.trace(e);
-        }
-        }
-        
-        public static getTeams = async (req: Request, res: Response) => {
-            res.send(MatchController.teams);
-        }
-        
-        private static async loadMatches() {
-            const request = await fetch(`https://api.football-data.org/v2/competitions/${EUROPEAN_CHAMPIONSHIP_ID}/matches?season=${CURRENT_SEASON_YEAR}`, {
-                headers: {
-                    "X-Auth-Token": "79fe8894898e4ca6aee7c1fd9142f91c",
+            return match;
+        }));
+    }
+    
+    public static getTeams = async (req: Request, res: Response) => {
+        res.send(MatchController.teams);
+    }
+    
+    private static async loadMatches() {
+        const request = await fetch(`https://api.football-data.org/v2/competitions/${EUROPEAN_CHAMPIONSHIP_ID}/matches?season=${CURRENT_SEASON_YEAR}`, {
+            headers: {
+                "X-Auth-Token": "79fe8894898e4ca6aee7c1fd9142f91c",
             },
         });
         this.matches = (await request.json())?.matches.map((m: Match, i) => {
