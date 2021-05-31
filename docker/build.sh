6a0d5a06ca63b17a3cb021b84e3616e6b09a8b2c
Hannes Rüger <hannesrueger@gmx.de>
2021-05-31 18:33:05 +0200

    (docker) fixed build script

--- docker/build.sh
+++ docker/build.sh
@@ -6,7 +6,6 @@ npm install
 npx @zeit/ncc build src/index.ts -o ../docker/backend
 cd ..
 cp ./container-env.json ./docker/container-env.json
-cp ./api/assets/worldmap.png ./docker/worldmap.png
 cd ./docker
 PACKAGE_VERSION=$(cat ../AGTippspiel/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 docker build -t hrueger/agtippspiel:v$PACKAGE_VERSION .
